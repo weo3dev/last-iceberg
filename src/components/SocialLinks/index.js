c7f8f444309bffb403ee50e55ed80917a641a3a3
Billy ONeal <dev@weo3.com>
2020-11-18 08:40:02 -0800

    Working; all things trimmed

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,32 +1,47 @@
 import React from 'react';
 
-import { Twitter } from 'styled-icons/boxicons-logos/Twitter';
-import { Github } from 'styled-icons/boxicons-logos/Github';
-
-import * as S from './styled';
+import fbLogo from '../../images/logos/facebook.png';
+import twLogo from '../../images/logos/twitter.png';
+import igLogo from '../../images/logos/instagram.png';
+import ytLogo from '../../images/logos/youtube.png';
 
 const SocialLinks = () => {
   return (
-    <S.SocialLinksList>
-      <S.SocialLinksItem>
-        <S.SocialLinksLink
-          href="https://github.com/diogorodrigues/iceberg-gatsby-multilang"
-          title="Github"
-          target="_blank"
-        >
-          <Github />
-        </S.SocialLinksLink>
-      </S.SocialLinksItem>
-      <S.SocialLinksItem>
-        <S.SocialLinksLink
-          href="https://twitter.com/_diogorodrigues"
+    <ul className="social">
+      <li>
+        <a
+          href="https://twitter.com/playing4change"
           title="Twitter"
           target="_blank"
         >
-          <Twitter />
-        </S.SocialLinksLink>
-      </S.SocialLinksItem>
-    </S.SocialLinksList>
+          <img src={twLogo} />
+        </a>
+      </li>
+      <li>
+        <a href="https://facebook.com/PlayingForChange"
+           title="Facebook"
+           target="_blank"
+        >
+          <img src={fbLogo} />
+        </a>
+      </li>
+      <li>
+        <a href="https://instagram.com/playing4change"
+           title="Instagram"
+           target="_blank"
+        >
+          <img src={igLogo} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/c/playingchange"
+           title="YouTube"
+           target="_blank"
+        >
+          <img src={ytLogo} />
+        </a>
+      </li>
+    </ul>
   );
 };
 
--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import GlobalStyles from '../styles/global';
-
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
 import { useLocale } from '../hooks/locale';
-
-import * as S from './styled';
 
 const BaseLayout = ({ children, pageContext: { locale } }) => {
   // Using the useLocale() hook to define the correct locale 
@@ -16,14 +11,9 @@ const BaseLayout = ({ children, pageContext: { locale } }) => {
 
   return (
     <>
-      <GlobalStyles />
-      <S.Wrapper>
-        <Header />
-        <S.SiteContent role="main">
-          <S.Container>{children}</S.Container>
-        </S.SiteContent>
-        <Footer />
-      </S.Wrapper>
+      <Header />
+      {children}
+      <Footer />
     </>
   )
 };
